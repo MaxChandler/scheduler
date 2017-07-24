48089a317dc7b804a5fea11c4d273ecdc599b802
Max Chandler <chandlerm1@cardiff.ac.uk>
2017-07-24 16:24:59 +0100

    if tmux window already exists, dont try and create it again.

--- cron_scheduler.sh
+++ cron_scheduler.sh
@@ -7,6 +7,8 @@ if !(( $(pgrep -c -u $USER -f scheduler_handler.sh) > 1 )) ; then
 		tmux start-server
 		tmux new-session -d 
 	fi
-	tmux new-window -n "$TMUX_WINDOW_NAME"
+	if ! tmux list-windows | grep "$TMUX_WINDOW_NAME">/dev/null; then
+		tmux new-window -n "$TMUX_WINDOW_NAME"
+	fi
 	tmux send-keys -t "$TMUX_WINDOW_NAME" "/home/$USER/scripts/scheduler_handler.sh" C-m
 fi
