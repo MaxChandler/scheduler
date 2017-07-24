d8a7b758ac5ce82d94bcbb63e9cdb62748be4d48
Max Chandler <chandlerm1@cardiff.ac.uk>
2017-07-24 16:22:41 +0100

    if tmux is already running, we add a window. error in if statement

--- cron_scheduler.sh
+++ cron_scheduler.sh
@@ -3,10 +3,10 @@ declare -r TMUX_WINDOW_NAME='scheduler_handler'
 
 if !(( $(pgrep -c -u $USER -f scheduler_handler.sh) > 1 )) ; then
 	if ! tmux ls > /dev/null 2>&1 ; then
+		# start new tmux session
 		tmux start-server
 		tmux new-session -d 
-		# start new tmux session
-		tmux new-window -n "$TMUX_WINDOW_NAME"
-		tmux send-keys -t "$TMUX_WINDOW_NAME" "/home/$USER/scripts/scheduler_handler.sh" C-m
 	fi
+	tmux new-window -n "$TMUX_WINDOW_NAME"
+	tmux send-keys -t "$TMUX_WINDOW_NAME" "/home/$USER/scripts/scheduler_handler.sh" C-m
 fi
