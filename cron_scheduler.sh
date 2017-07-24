b8bc02baf673dc496a854ea46dfc1538ea6aeee0
Max Chandler <chandlerm1@cardiff.ac.uk>
2017-07-24 16:35:00 +0100

    added cd path change to cron

--- cron_scheduler.sh
+++ cron_scheduler.sh
@@ -4,6 +4,7 @@ declare -r TMUX_WINDOW_NAME='handler'
 if (( $(pgrep -c -u $USER -f scheduler_handler.sh) > 1 )) ; then
 	:
 else
+	cd ~/scripts
 	if ! tmux ls > /dev/null 2>&1 ; then
 		# start new tmux session
 		tmux new-session -d 
@@ -11,5 +12,5 @@ else
 	if ! tmux list-windows | grep "$TMUX_WINDOW_NAME">/dev/null; then
 		tmux new-window -n "$TMUX_WINDOW_NAME"
 	fi
-	tmux send-keys -t "$TMUX_WINDOW_NAME" "/home/$USER/scripts/scheduler_handler.sh" C-m
+	tmux send-keys -t "$TMUX_WINDOW_NAME" "scheduler_handler.sh" C-m
 fi
